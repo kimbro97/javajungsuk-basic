231d6bfb529157aa8741daeede8b12b1611ff8ca
kimbro97 <rlagudwog@gmail.com>
2023-02-22 21:17:26 +0900

    자바의 정석 OOP2

---
+++ oop2/src/Ex7_6.java
@@ -0,0 +1,9 @@
+import static java.lang.Math.*;
+
+public class Ex7_6 {
+    public static void main(String[] args) {
+        System.out.println(random());
+        System.out.println(PI);
+
+    }
+}
---
+++ oop2/src/Ex7_7.java
@@ -0,0 +1,13 @@
+public class Ex7_7 {
+    public static void main(String[] args) {
+        a a = new a();
+//        a.b;
+    }
+}
+class a{
+    private int b = 0;
+}
+/**
+ * 접근제어자 public, protected, (default), private
+ * 그외제어자 static, final, abstract, native, transient, synchronized, volatile, strictfp
+ */
