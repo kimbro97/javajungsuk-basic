eb5fbc658469d8de026e43d9172b767ff8f880a6
kimbro97 <rlagudwog@gmail.com>
2023-10-16 19:27:35 +0900

    Ex11_2 예제

---
+++ collections/src/Ex11_2.java
@@ -0,0 +1,28 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class Ex11_2 {
+    public static void main(String[] args) {
+        Stack stack = new Stack();
+        Queue q = new LinkedList();
+
+        stack.push("0");
+        stack.push("1");
+        stack.push("2");
+
+        q.offer("0");
+        q.offer("1");
+        q.offer("2");
+        System.out.println("= stack =");
+
+        while (!stack.empty()) {
+            System.out.println(stack.pop());
+        }
+
+        System.out.println("= Queue =");
+        while (!q.isEmpty()) {
+            System.out.println(q.poll());
+        }
+    }
+}
