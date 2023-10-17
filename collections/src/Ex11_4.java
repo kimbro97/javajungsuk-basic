db59aaea53118370985b1206a59e37b5914e6dfb
kimbro97 <rlagudwog@gmail.com>
2023-10-17 20:46:55 +0900

    Ex11_4 예제

--- collections/src/Ex11_4.java
+++ collections/src/Ex11_4.java
@@ -8,33 +8,37 @@ public class Ex11_4 {
     static final int MAX_SIZE = 5;
 
     public static void main(String[] args) {
-        System.out.println(">>");
-        try {
-            Scanner scanner = new Scanner(System.in);
-            String input = scanner.nextLine().trim();
+        while (true) {
+            System.out.println(">>");
+            try {
+                Scanner scanner = new Scanner(System.in);
+                String input = scanner.nextLine().trim();
 
-            if (input.equalsIgnoreCase("q")) {
-                System.exit(0);
-            } else if (input.equalsIgnoreCase("help")) {
-                System.out.println(" help - 도움말을 보여줍니다");
-                System.out.println(" q 또는 Q - 프로그램을 종료합니다");
-                System.out.println(" history - 최근에 입력한 명령어를" + MAX_SIZE + "개 보여줍니다.");
-            } else if (input.equalsIgnoreCase("history")) {
-                int i = 0;
-                save(input);
+                if ("".equals(input)) continue;
 
-                LinkedList tmp = (LinkedList) q;
-                ListIterator it = tmp.listIterator();
+                if (input.equalsIgnoreCase("q")) {
+                    System.exit(0);
+                } else if (input.equalsIgnoreCase("help")) {
+                    System.out.println(" help - 도움말을 보여줍니다");
+                    System.out.println(" q 또는 Q - 프로그램을 종료합니다");
+                    System.out.println(" history - 최근에 입력한 명령어를" + MAX_SIZE + "개 보여줍니다.");
+                } else if (input.equalsIgnoreCase("history")) {
+                    int i = 0;
+                    save(input);
 
-                while (it.hasNext()) {
-                    System.out.println(++i + "." + it.next());
+                    LinkedList tmp = (LinkedList) q;
+                    ListIterator it = tmp.listIterator();
+
+                    while (it.hasNext()) {
+                        System.out.println(++i + "." + it.next());
+                    }
+                } else {
+                    save(input);
+                    System.out.println(input);
                 }
-            } else {
-                save(input);
-                System.out.println(input);
+            } catch (Exception e) {
+                System.out.println("입력오류입니다.");
             }
-        } catch (Exception e) {
-            System.out.println("입력오류입니다.");
         }
     }
 
