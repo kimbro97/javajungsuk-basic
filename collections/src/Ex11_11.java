d3c703c137e86620600b386b3b0934f35ba8ac26
kimbro97 <rlagudwog@gmail.com>
2023-10-19 20:56:34 +0900

    Ex11_11 예제

---
+++ collections/src/Ex11_11.java
@@ -0,0 +1,42 @@
+import java.util.*;
+
+public class Ex11_11 {
+    public static void main(String[] args) {
+        HashSet set = new HashSet();
+
+        set.add("abc");
+        set.add("abc");
+        set.add(new Person("David",10));
+        set.add(new Person("David",10));
+
+        System.out.println(set);
+    }
+}
+
+class Person {
+    String name;
+    int age;
+
+    public Person(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return name +":"+ age;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return age == person.age && Objects.equals(name, person.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(name, age);
+    }
+}
